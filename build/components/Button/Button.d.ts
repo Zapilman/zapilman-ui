c6fa96a54a2424161af6f320db2b8a032584275c
Zapilman <17uraz12@gmail.com>
2023-05-21 06:59:54 +0300

    remove build

--- build/components/Button/Button.d.ts
+++
@@ -1,6 +0,0 @@
-import { type FC } from 'react';
-interface Props {
-    title: string;
-}
-declare const Button: FC<Props>;
-export default Button;
--- build/components/Input/Input.d.ts
+++
@@ -1,2 +0,0 @@
-declare const Input: () => JSX.Element;
-export default Input;
