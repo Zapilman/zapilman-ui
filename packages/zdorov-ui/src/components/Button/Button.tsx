3753f80c8ef145dcf07d179d235c97f1fe52515a
Zapilman <17uraz12@gmail.com>
2023-05-22 18:22:43 +0300

    update btn

--- packages/zdorov-ui/src/components/Button/Button.tsx
+++ packages/zdorov-ui/src/components/Button/Button.tsx
@@ -1,9 +1,10 @@
-import React, { type FC } from 'react';
+import React, { type FC, type ReactNode } from 'react';
 
 import './Button.css';
 
 export interface Props {
   title: string;
+  children?: ReactNode;
 }
 
 const Button: FC<Props> = ({ title }) => {
