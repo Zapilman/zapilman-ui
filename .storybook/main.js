c2b4bf41ad05fee7fcc0e2b937979f2ff96e2637
Zapilman <17uraz12@gmail.com>
2023-06-03 10:40:18 +0300

    remove lerna

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,5 @@
 module.exports = {
-  stories: [
-    '../packages/zdorov-ui/src/**/*.stories.mdx',
-    '../packages/zdorov-ui/src/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,50 @@
-const path = require('path');
+import { resolve as _resolve } from 'path';
 
-module.exports = {
-  mode: 'production',
-  entry: './packages/zdorov-ui/src/index.ts',
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'packages/zdorov-ui/dist'),
-    libraryTarget: 'umd',
-    clean: true,
-  },
-  resolve: {
-    modules: [__dirname, 'src', 'node_modules'],
-    extensions: ['*', '.js', '.jsx', '.tsx', '.ts', '...'],
-  },
-  externals: {
-    react: 'react',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: require.resolve('babel-loader'),
-      },
-      {
-        test: /\.s[ac]ss|css$/i,
-        use: [
-          { loader: 'style-loader' },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                localIdentName: '[name]__[local]___[hash:base64:5]',
-              },
+export const mode = 'production';
+export const entry = './src/index.ts';
+export const output = {
+  filename: 'index.js',
+  path: _resolve(__dirname, 'dist'),
+  libraryTarget: 'umd',
+  clean: true,
+};
+export const resolve = {
+  modules: [__dirname, 'src', 'node_modules'],
+  extensions: ['*', '.js', '.jsx', '.tsx', '.ts', '...'],
+};
+export const externals = {
+  react: 'react',
+};
+export const module = {
+  rules: [
+    {
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: require.resolve('babel-loader'),
+    },
+    {
+      test: /\.s[ac]ss|css$/i,
+      use: [
+        { loader: 'style-loader' },
+        {
+          loader: 'css-loader',
+          options: {
+            modules: {
+              localIdentName: '[name]__[local]___[hash:base64:5]',
             },
           },
-          { loader: 'sass-loader' },
-        ],
-      },
-      {
-        test: /\.png|svg|jpg|gif$/,
-        use: ['file-loader'],
-      },
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
+        },
+        { loader: 'sass-loader' },
+      ],
+    },
+    {
+      test: /\.png|svg|jpg|gif$/,
+      use: ['file-loader'],
+    },
+    {
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/,
+    },
+  ],
 };
